06adb4213f2c040ace9727b4c01cb23281cac617
Hugo Rincon <hugo@bakedweb.net>
2011-08-11 19:25:42 -0430

    generate code

--- public/js/app.js
+++ public/js/app.js
@@ -3,4 +3,33 @@ $(document).ready(function() {
 
   $('.ayuda').hide();
 
+  $('#btn_generar_cod').click(function(){
+	generate_code($('#tu_pagina').val());
+    $('#thankyou_right').slideDown();
+  }); 
+
+  //actualiza script
+  $('#tu_pagina').keyup(function(){
+    generate_code($(this).val());
+  }); 
+
 });
+
+function generate_code(valor){
+	codigo = '<p>&#60;script&#62;</p>';
+	codigo += '<p>Esturistico = new Object();</p>';
+	codigo += '<p>Esturistico.tag = "' + get_tag(valor) + '"&#59; </p>';
+	codigo += '<p>Esturistico.redirect = "' + get_redirect(valor) + '"&#59; </p>';	
+	codigo += '<p>&#60;/script&#62;</p>';
+	codigo += '<p>&#60;script src="http://www.esturisti.co/javascripts/cortina_external.js" &#62;</p>';
+	$('.pega_code').html(codigo);
+}
+
+function get_tag(url){
+	url = url.split('.');
+	return url[url.length - 2];  //devuelve el valor antes del .com, .net. etc...
+}
+
+function get_redirect(url){
+	return url.replace("http://", "").replace("www.","").replace(" ", ""); //remuevo dato inecesario del url
+}
