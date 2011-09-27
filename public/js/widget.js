8f883cc9ad84e959aad8fc4732211c32964812ed
Hugo Rincon <hugo@bakedweb.net>
2011-09-27 16:05:22 -0430

    refactor widget

--- public/js/widget.js
+++ public/js/widget.js
@@ -2,35 +2,23 @@
   var VenueDriver;
   VenueDriver = (function() {
     function VenueDriver() {}
-    VenueDriver.API_USERNAME = "ivanacostarubio";
-    VenueDriver.API_PASSWORD = "letmein";
-    VenueDriver.VENUE_URL = "http://venuedriver.com/api/accounts/44/venues.json?username=" + VenueDriver.API_USERNAME + "&password=" + VenueDriver.API_PASSWORD + "&callback=?";
+    VenueDriver.API_TOKEN = "NVJEZ3T8A861Q2";
+    VenueDriver.host = "venuedriver.com";
+    VenueDriver.VENUE_URL = "http://" + VenueDriver.host + "/api/accounts/93/all_events?token=" + VenueDriver.API_TOKEN + "&callback=?";
     VenueDriver.venues_list = new Array();
     VenueDriver.prototype.venues = function() {
-      return $.getJSON(VenueDriver.VENUE_URL, function(data) {
-        $("#loading").css('display', '');
-        return $.each(data, function(i, item) {
-          VenueDriver.events(item);
-          if (i === (data.length - 1)) {
-            return $("#loading").css('display', 'none');
-          }
+      $("#loading").css('display', '');
+      $.getJSON(VenueDriver.VENUE_URL, function(data) {
+        $('#main_table').append("<tbody></tbody>");
+        $.each(data, function(i, item) {
+          var table_data;
+          table_data = "<tr>                  <td width='92' class='eventitem_text'>" + item.date + "</td>                  <td width='136' calss='eventitem_text'>Venue Location</td>                  <td width='130' class='eventitem_text'>Venue Title</td>                  <td class='eventitem_text'>" + item.title + "</td>                  <td align='right'><a href='venue.home_URL/buy/tickets/event/" + item.id + "'>Buy</a></td>                  </tr>";
+          $('#main_table tbody').append(table_data);
         });
+        $("#loading").css('display', 'none');
+        $("#main_table").colorize({hiliteColor: '#CFCFCF'});  
+        $("#main_table").tablesorter();
       });
-    };
-    VenueDriver.events = function(venue) {
-      $.ajax({
-        url: "http://venuedriver.com/api/venues/" + venue.id + "/events.json?username=" + this.API_USERNAME + "&password=" + this.API_PASSWORD + "&callback=?",
-        dataType: 'jsonp',
-        cache: true,
-        success: function(data) {
-          return $.each(data, function(i, item) {
-            var table_data;
-            table_data = "<div id='selectevent_eventitem'><table width='100%' height='49px' cellspacing='0' cellpadding='0'><tr>                  <td width='92' class='eventitem_text'>" + item.date + "</td>                  <td width='136' calss='eventitem_text'>" + venue.city + ", " + venue.state + "</td>                  <td width='130' class='eventitem_text'>" + venue.title + "</td>                  <td class='eventitem_text'>" + item.title + "</td>                  <td align='right'><a href='" + venue.home_URL + "/buy/tickets/event/" + item.id + "'>Buy</a></td>                  </tr></table></div>";
-            return $('#wrapper').append(table_data);
-          });
-        }
-      });
-      return true;
     };
     return VenueDriver;
   })();
