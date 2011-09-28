29ee7aca1819871f7a96e09724825831d9ab4c55
Hugo Rincon <hugo@bakedweb.net>
2011-09-28 17:29:32 -0430

    refactor - first step

--- public/js/widget.js
+++ public/js/widget.js
@@ -2,30 +2,44 @@
   var VenueDriver;
   VenueDriver = (function() {
     function VenueDriver() {}
-    VenueDriver.API_TOKEN = "NVJEZ3T8A861Q2";
-    VenueDriver.host = "venuedriver.com";
-    VenueDriver.VENUE_URL = "http://" + VenueDriver.host + "/api/accounts/44/all_events?token=" + VenueDriver.API_TOKEN + "&callback=?";
-    VenueDriver.venues_list = new Array();
-    VenueDriver.prototype.venues = function() {
-      $.getJSON(VenueDriver.VENUE_URL, function(data) {
-        $('#main_table').append("<tbody></tbody>");
-        $.each(data, function(i, item) {
-          var table_data;
-          $.each(item.events, function(i, event){
-            table_data = "<tr>                  <td width='92' class='eventitem_text'>" + event.date + "</td>                  <td width='136' calss='eventitem_text'>" + item.venue.city + ", " + item.venue.state +"</td>                  <td width='130' class='eventitem_text'>" + item.venue.title + "</td>                  <td class='eventitem_text'>" + event.title + "</td>                  <td align='right'><a href='" + item.venue.home_URL + "buy/tickets/event/" + event.id + "'>Buy</a></td>                  </tr>";
-            $('#main_table tbody').append(table_data);
-          });
-        });
-        $("#loading").css('display', 'none'); 
-        $("#main_table").tablesorter({sortList: [[0,0]]}); //sort table
-        $("#main_table").colorize({hiliteColor: '#CFCFCF'}); 
+
+    //Config
+    $(function initialize(){
+      VenueDriver.api_token = "NVJEZ3T8A861Q2";
+      VenueDriver.account = '93';
+      VenueDriver.host = "venuedriver.com";
+      VenueDriver.api_url = "http://" + VenueDriver.host + "/api/accounts/" + VenueDriver.account + "/all_events?token=" + VenueDriver.api_token + "&callback=?";
+    });
+    
+    VenueDriver.prototype.start = function(){
+      $.getJSON(VenueDriver.api_url, function(data) { 
+        VenueDriver.fill_table(data);
       });
     };
+    
+    VenueDriver.fill_table = function(data) {
+      $('#main_table').append("<tbody></tbody>");
+      $.each(data, function(i, item) {
+        var table_data;
+        $.each(item.events, function(i, event){
+          table_data = "<tr>                  <td width='92' class='eventitem_text'>" + event.date + "</td>                  <td width='136' calss='eventitem_text'>" + item.venue.city + ", " + item.venue.state +"</td>                  <td width='130' class='eventitem_text'>" + item.venue.title + "</td>                  <td class='eventitem_text'>" + event.title + "</td>                  <td align='right'><a href='" + item.venue.home_URL + "buy/tickets/event/" + event.id + "'>Buy</a></td>                  </tr>";
+          $('#main_table tbody').append(table_data);
+        });
+      });
+      $("#loading").css('display', 'none'); 
+      VenueDriver.style_table();
+    };
+    
+    VenueDriver.style_table = function() {
+      $("#main_table").tablesorter({sortList: [[0,0]]}); //sort table
+      $("#main_table").colorize({hiliteColor: '#CFCFCF'});
+    };
+    
     return VenueDriver;
   })();
   $(function() {
     var venuedriver;
     venuedriver = new VenueDriver;
-    return console.log(venuedriver.venues());
+    return console.log(venuedriver.start());
   });
 }).call(this);
