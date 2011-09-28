e312ed84f4846f6e0780fbf7c2b2a3cf840f57a4
Hugo Rincon <hugo@bakedweb.net>
2011-09-28 09:47:20 -0430

    add venue data & order by date

--- public/js/widget.js
+++ public/js/widget.js
@@ -4,7 +4,7 @@
     function VenueDriver() {}
     VenueDriver.API_TOKEN = "NVJEZ3T8A861Q2";
     VenueDriver.host = "venuedriver.com";
-    VenueDriver.VENUE_URL = "http://" + VenueDriver.host + "/api/accounts/93/all_events?token=" + VenueDriver.API_TOKEN + "&callback=?";
+    VenueDriver.VENUE_URL = "http://" + VenueDriver.host + "/api/accounts/44/all_events?token=" + VenueDriver.API_TOKEN + "&callback=?";
     VenueDriver.venues_list = new Array();
     VenueDriver.prototype.venues = function() {
       $("#loading").css('display', '');
@@ -12,12 +12,14 @@
         $('#main_table').append("<tbody></tbody>");
         $.each(data, function(i, item) {
           var table_data;
-          table_data = "<tr>                  <td width='92' class='eventitem_text'>" + item.date + "</td>                  <td width='136' calss='eventitem_text'>Venue Location</td>                  <td width='130' class='eventitem_text'>Venue Title</td>                  <td class='eventitem_text'>" + item.title + "</td>                  <td align='right'><a href='venue.home_URL/buy/tickets/event/" + item.id + "'>Buy</a></td>                  </tr>";
-          $('#main_table tbody').append(table_data);
+          $.each(item.events, function(i, event){
+            table_data = "<tr>                  <td width='92' class='eventitem_text'>" + event.date + "</td>                  <td width='136' calss='eventitem_text'>" + item.venue.city + ", " + item.venue.state +"</td>                  <td width='130' class='eventitem_text'>" + item.venue.title + "</td>                  <td class='eventitem_text'>" + event.title + "</td>                  <td align='right'><a href='" + item.venue.home_URL + "buy/tickets/event/" + event.id + "'>Buy</a></td>                  </tr>";
+            $('#main_table tbody').append(table_data);
+          });
         });
         $("#loading").css('display', 'none');
         $("#main_table").colorize({hiliteColor: '#CFCFCF'});  
-        $("#main_table").tablesorter();
+        $("#main_table").tablesorter({sortList: [[0,0]]}); //sort table
       });
     };
     return VenueDriver;
